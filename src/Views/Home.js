6de6c594bc43f1f65d3bdec72ee68d617f738ecd
¨santisega¨ <santisega2016@gmail.com>
2022-12-19 18:57:23 -0300

    ¨avance1¨

---
+++ src/Views/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './home.css';
+
+function Home () {
+    return(
+        <div className="home">
+            <App>
+                <div>
+                    <p>Hola</p>
+                </div>
+            </App>
+        </div>
+    );
+}
+
+export default Home;
